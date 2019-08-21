dceb63a9901afe1f76ad03ec8dcc6d394d0fc50b
Reynaldo Gil <gilcu2@gmail.com>
2019-08-21 09:39:12 +0100

    finish wrapper methods

--- feature_selection/feature_selection.py
+++ feature_selection/feature_selection.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import statsmodels.api as sm
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.feature_selection import RFE
 
 
 def select_pearson_correlation(df: pd.DataFrame):
@@ -25,20 +29,73 @@ def select_pearson_correlation(df: pd.DataFrame):
     plt.show()
 
 
-def select_wrapper(df: pd.DataFrame):
-    features = df.drop(target_name)
+def backward_elimination(df: pd.DataFrame, features_names, taget_name):
+    features = df[features_names]
     target = df[target_name]
 
-    feature_1 = sm.add_constant(features)
-    model = sm.OLS(target, feature_1).fit()
-    print("Ordinary least squares:", model.pvalues)
+    pmax = 1
+    cols = list(features.columns)
+    while (len(cols) > 0):
+        p = []
+        X_1 = features[cols]
+        X_1 = sm.add_constant(X_1)
+        model = sm.OLS(target, X_1).fit()
+        p = pd.Series(model.pvalues.values[1:], index=cols)
+        pmax = max(p)
+        feature_with_p_max = p.idxmax()
+        if (pmax > 0.05):
+            cols.remove(feature_with_p_max)
+        else:
+            break
+
+    selected_features_BE = cols
+    print('Selected_features', selected_features_BE)
+
+
+def recursive_elimination(df: pd.DataFrame, features_names, taget_name):
+    X = df[features_names]
+    y = df[target_name]
+
+    nof_list = np.arange(1, 6)
+    high_score = 0
+    # Variable to store the optimum features
+    nof = 0
+    score_list = []
+    for n in range(len(nof_list)):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+        model = LinearRegression()
+        rfe = RFE(model, nof_list[n])
+        X_train_rfe = rfe.fit_transform(X_train, y_train)
+        X_test_rfe = rfe.transform(X_test)
+        model.fit(X_train_rfe, y_train)
+        score = model.score(X_test_rfe, y_test)
+        score_list.append(score)
+        if (score > high_score):
+            high_score = score
+            nof = nof_list[n]
+    print("Optimum number of features: %d" % nof)
+    print("Score with %d features: %f" % (nof, high_score))
+
+    cols = list(X.columns)
+    model = LinearRegression()
+    # Initializing RFE model
+    rfe = RFE(model, nof)
+    # Transforming data using RFE
+    X_rfe = rfe.fit_transform(X, y)
+    # Fitting the data to model
+    model.fit(X_rfe, y)
+    temp = pd.Series(rfe.support_, index=cols)
+    selected_features_rfe = temp[temp == True].index
+    print(selected_features_rfe)
 
 
 if __name__ == '__main__':
     path_CSV = "../data/spot-rCsvSpotProb.csv"
+    features_names = ['A', 'C', 'StF', 'SoF', 'N', 'MG']
     target_name = 'BC'
 
     df = pd.read_csv(path_CSV)
 
     # select_pearson_correlation(df)
-    select_wrapper(df)
+    # backward_elimination(df, features_names, target_name)
+    recursive_elimination(df, features_names, target_name)
--- feature_selection/preprocessing.py
+++ feature_selection/preprocessing.py
@@ -1,18 +1,27 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from pandas.plotting import radviz
 
 if __name__ == '__main__':
     path_Excel = "../data/spot-rCsvSpotProb.xlsx"
     path_CSV = "../data/spot-rCsvSpotProb.csv"
+    target_map = {2: 0, 5: 1}
 
     df = pd.read_excel(path_Excel, usecols=['A', 'C', 'StF', 'SoF', 'N', 'MG', 'BC'])
+    df['BC'] = df['BC'].map(target_map)
     df.to_csv(path_CSV)
-    df.hist()
-    plt.show()
+
     descriptions = df.describe(include='all')
     for d in descriptions:
         print(d, '\n', descriptions[d])
 
     df.to_csv(path_CSV)
-    df.hist()
+
+    # df.hist()
+    # df.plot(subplots=True)
+    # radviz(df, 'BC', color=['red', 'green'])
+    # plt.show()
+
+    df_subset = df[['A', 'C', 'N', 'SoF', 'MG', 'BC']]
+    radviz(df_subset, 'BC', color=['red', 'green'])
     plt.show()
